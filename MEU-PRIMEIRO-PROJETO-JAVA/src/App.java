734cded831278ac125955a1cb5ebcabb9c8bfda8
JoãoCampos <joaocamposwork@gmail.com>
2024-05-15 23:41:25 -0300

    MEU PRIMEIRO PROJETO JAVA

---
+++ MEU-PRIMEIRO-PROJETO-JAVA/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("OLA SEJAM BEM VINDOS NO MEU PRIMEIRO CODIGO EM JAVA , PELO CURSO DA DIO DE BACK END COM JAVA!");
+    }
+}
